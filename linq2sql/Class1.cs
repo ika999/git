d9f2113ed19dc945e385354c988dfcf7cc039fa7
ika999 <ivan.krasic9@gmail.com>
2013-09-14 23:31:37 +0200

    projekat linq2sql
    prva verzija

---
+++ linq2sql/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linq2sql
+{
+    public class Class1
+    {
+        //public void Linq76()
+        //{
+        //    List<Customer> customers = GetCustomerList();
+
+        //    var orderCounts =
+        //        from c in customers
+        //        select new { c.CustomerID, OrderCount = c.Orders.Count() };
+
+        //    ObjectDumper.Write(orderCounts);
+        //}
+    }
+}
